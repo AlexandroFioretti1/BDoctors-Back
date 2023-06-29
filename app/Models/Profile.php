0265818a9b4826e4930e86542650ed3f9d361113
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-06-29 16:21:31 +0200

    add models

---
+++ app/Models/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+    'slug','phone_number','address','doctor_image','cv','perfomances','user_id'
+    ];
+}
