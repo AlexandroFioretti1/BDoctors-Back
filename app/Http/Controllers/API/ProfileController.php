c4e133552af66feb5664500e48fc74130de8dc66
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-07-04 16:27:22 +0200

    add route api and fix name tables profile controller

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -11,7 +11,7 @@ class ProfileController extends Controller
     public function index()
     {
         //get all profile  by paginate with tables connect 'rewies','votes','specializzation','user'
-        $profiles = Profile::with('rewiews','votes','specializzation','user')->paginate(10);
+        $profiles = Profile::with('reviews','votes','specializations','user')->paginate(10);
 
         //return file json with status success and $profiles
         return response()->json([
