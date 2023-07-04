f79941ed081e7bdf617dc42d4a718e74fc5a694f
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-04 12:22:22 +0200

    add required register validation on js

--- public/js/checkErrorRegister.js
+++ public/js/checkErrorRegister.js
@@ -1,86 +1,108 @@
 document
     .getElementById("my-form-register")
     .addEventListener("submit", function (event) {
+        function checkError(input, error) {
+            event.preventDefault();
+            const errorMessage = input.validationMessage;
+            error.classList.remove("d-none");
+            console.log("Errore di validazione:", errorMessage);
+            input.classList.add("error");
+        }
+
+        function checkErrorRequired(inputValue, input, error) {
+            if (inputValue == "") {
+                checkError(input, error);
+            }
+        }
+
         /* name */
         const nameInput = document.getElementById("name");
         const nameInputValue = nameInput.value;
         const nameError = document.getElementById("nameError");
+        const nameErrorRequired = document.getElementById("nameErrorRequired");
 
         // surname
 
         const surnameInput = document.getElementById("surname");
         const surnameInputValue = surnameInput.value;
         const surnameError = document.getElementById("surnameError");
+        const surnameErrorRequired = document.getElementById(
+            "surnameErrorRequired"
+        );
 
         //address
         const addressInput = document.getElementById("address");
         const addressInputValue = addressInput.value;
         const addressError = document.getElementById("addressError");
+        const addressErrorRequired = document.getElementById(
+            "addressErrorRequired"
+        );
 
         //Email
         const emailInput = document.getElementById("email");
         const emailInputValue = emailInput.value;
         const emailError = document.getElementById("emailError");
+        const emailErrorRequired =
+            document.getElementById("emailErrorRequired");
 
         //Password
         const PasswordInput = document.getElementById("password");
         const PasswordInputValue = PasswordInput.value;
         const PasswordError = document.getElementById("passwordError");
+        const passwordErrorRequired = document.getElementById(
+            "passwordErrorRequired"
+        );
 
         //confirmPassword
         const confirmPasswordInput =
             document.getElementById("password-confirm");
         const confirmPasswordInputValue = confirmPasswordInput.value;
 
-        //name validation
-        if (nameInputValue.length > 50) {
-            // Prevent form sent
-            event.preventDefault();
-            const errorMessage = nameInput.validationMessage;
-            nameError.classList.remove("d-none");
-            console.log("Errore di validazione 1:", errorMessage);
-            nameInput.classList.add("error");
+        //check required
+
+        //surname
+        checkErrorRequired(nameInputValue, nameInput, nameErrorRequired);
+        checkErrorRequired(
+            surnameInputValue,
+            surnameInput,
+            surnameErrorRequired
+        );
+
+        //address
+        checkErrorRequired(
+            addressInputValue,
+            addressInput,
+            addressErrorRequired
+        );
+
+        //password
+        checkErrorRequired(emailInputValue, emailInput, emailErrorRequired);
+        checkErrorRequired(
+            PasswordInputValue,
+            PasswordInput,
+            passwordErrorRequired
+        );
+
+        //name, surname and address validation
+        function checkErrorlenght(inputValue, input, error, length) {
+            if (inputValue.length > length) {
+                checkError(input, error);
+            }
         }
 
-        //surname validation
-        if (surnameInputValue.length > 50) {
-            // Prevent form sent
-            event.preventDefault();
-            const errorMessage = surnameInput.validationMessage;
-            surnameError.classList.remove("d-none");
-            console.log("Errore di validazione 2:", errorMessage);
-            surnameInput.classList.add("error");
-        }
-
-        //address validation
-        if (addressInputValue.length > 255) {
-            // Prevent form sent
-            event.preventDefault();
-            const errorMessage = addressInput.validationMessage;
-            addressError.classList.remove("d-none");
-            console.log("Errore di validazione 3:", errorMessage);
-            addressInput.classList.add("error");
-        }
+        checkErrorlenght(nameInputValue, nameInput, nameError, 50);
+        checkErrorlenght(surnameInputValue, surnameInput, surnameError, 50);
+        checkErrorlenght(addressInputValue, addressInput, addressError, 255);
 
         //password validation
         if (PasswordInputValue != confirmPasswordInputValue) {
-            // Prevent form sent
-            event.preventDefault();
-            const errorMessage = PasswordInput.validationMessage;
-            PasswordError.classList.remove("d-none");
-            console.log("Errore di validazione 4:", errorMessage);
-            PasswordInput.classList.add("error");
+            checkError(PasswordInput, PasswordError);
         }
 
         //email validation
 
         if (!emailInputValue.includes("@") || !emailInputValue.includes(".")) {
-            // Prevent form sent
-            event.preventDefault();
-            const errorMessage = emailInput.validationMessage;
-            emailError.classList.remove("d-none");
-            console.log("Errore di validazione 5:", errorMessage);
-            emailInput.classList.add("error");
+            checkError(emailInput, emailError);
         }
 
         //take At and Point index
@@ -89,31 +111,16 @@ document
 
         //check if there is text beetween @ and .
         if (emailInputValue.substring(positionAt, positionPoint) == "") {
-            //Prevent form sent
-            event.preventDefault();
-            const errorMessage = emailInput.validationMessage;
-            emailError.classList.remove("d-none");
-            console.log("Errore di validazione 6:", errorMessage);
-            emailInput.classList.add("error");
+            checkError(emailInput, emailError);
         }
 
         //check if there is text beetween start and @
         if (positionAt - 1 == 0) {
-            //Prevent form sent
-            event.preventDefault();
-            const errorMessage = emailInput.validationMessage;
-            emailError.classList.remove("d-none");
-            console.log("Errore di validazione 7:", errorMessage);
-            emailInput.classList.add("error");
+            checkError(emailInput, emailError);
         }
 
         //check if there is text beetween . and end
         if (positionPoint == emailInputValue.length - 1) {
-            //Prevent form sent
-            event.preventDefault();
-            const errorMessage = emailInput.validationMessage;
-            emailError.classList.remove("d-none");
-            console.log("Errore di validazione 8:", errorMessage);
-            emailInput.classList.add("error");
+            checkError(emailInput, emailError);
         }
     });
