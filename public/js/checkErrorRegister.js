5a03b94a172d35ebedce228f685241149039368f
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-04 10:23:49 +0200

    fix some validation repetion

--- public/js/checkErrorRegister.js
+++ public/js/checkErrorRegister.js
@@ -1,7 +1,6 @@
 document
     .getElementById("my-form-register")
     .addEventListener("submit", function (event) {
-        console.log("prova");
         /* name */
         const nameInput = document.getElementById("name");
         const nameInputValue = nameInput.value;
@@ -27,56 +26,44 @@ document
         const confirmPasswordInput =
             document.getElementById("password-confirm");
         const confirmPasswordInputValue = confirmPasswordInput.value;
-        const confirmPasswordError = document.getElementById(
-            "password-confirm-error"
-        );
 
         //name validation
         if (nameInputValue.length > 50) {
+            // Prevent form sent
+            event.preventDefault();
             const errorMessage = nameInput.validationMessage;
             nameError.classList.remove("d-none");
-            nameError.classList.add("d-block");
             console.log("Errore di validazione:", errorMessage);
             nameInput.classList.add("error");
-
-            // Previeni l'invio del form
-            event.preventDefault();
         }
 
         //surname validation
         if (surnameInputValue.length > 50) {
+            // Prevent form sent
+            event.preventDefault();
             const errorMessage = surnameInput.validationMessage;
             surnameError.classList.remove("d-none");
-            surnameError.classList.add("d-block");
             console.log("Errore di validazione:", errorMessage);
             surnameInput.classList.add("error");
-
-            // Previeni l'invio del form
-            event.preventDefault();
         }
 
         //address validation
         if (addressInputValue.length > 255) {
+            // Prevent form sent
+            event.preventDefault();
             const errorMessage = addressInput.validationMessage;
             addressError.classList.remove("d-none");
-            addressError.classList.add("d-block");
             console.log("Errore di validazione:", errorMessage);
             addressInput.classList.add("error");
-
-            // Previeni l'invio del form
-            event.preventDefault();
         }
 
         //password validation
-
         if (PasswordInputValue != confirmPasswordInputValue) {
+            // Prevent form sent
+            event.preventDefault();
             const errorMessage = PasswordInput.validationMessage;
             PasswordError.classList.remove("d-none");
-            PasswordError.classList.add("d-block");
             console.log("Errore di validazione:", errorMessage);
             PasswordInput.classList.add("error");
-
-            // Previeni l'invio del form
-            event.preventDefault();
         }
     });
