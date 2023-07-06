94808e99c08855029994b55401203369f1de4536
MarioBlasi <mblasi509@gmail.com>
2023-07-06 12:00:37 +0100

    seeder files reviews

---
+++ config/reviews.php
@@ -0,0 +1,124 @@
+<?php
+
+return [
+    [
+        "text" => "Ottima esperienza, il dottore è molto competente",
+        "date" => "2023-01-15 09:30:00",
+        "name" => "Mario",
+        "surname" => "Rossi",
+        "email" => "mario.rossi@example.com",
+        "profile_id" => 43
+    ],
+    [
+        "text" => "Mi sono trovato benissimo, lo consiglio a tutti",
+        "date" => "2023-02-02 14:45:00",
+        "name" => "Laura",
+        "surname" => "Bianchi",
+        "email" => "laura.bianchi@example.com",
+        "profile_id" => 43
+    ],
+    [
+        "text" => "Il dottore è stato molto gentile e professionale",
+        "date" => "2023-02-18 11:15:00",
+        "name" => "Giovanni",
+        "surname" => "Verdi",
+        "email" => "giovanni.verdi@example.com",
+        "profile_id" => 43
+    ],
+    [
+        "text" => "Mi sono sentito ascoltato e ho ricevuto un'ottima cura",
+        "date" => "2023-03-07 16:20:00",
+        "name" => "Paola",
+        "surname" => "Rizzo",
+        "email" => "paola.rizzo@example.com",
+        "profile_id" => 43
+    ],
+    [
+        "text" => "Il dottore è davvero bravo, ho risolto il mio problema",
+        "date" => "2023-03-22 10:00:00",
+        "name" => "Marco",
+        "surname" => "Esposito",
+        "email" => "marco.esposito@example.com",
+        "profile_id" => 43
+    ],
+    [
+        "text" => "Mi sono sentita accolta e in buone mani",
+        "date" => "2023-04-10 15:30:00",
+        "name" => "Sara",
+        "surname" => "Conti",
+        "email" => "sara.conti@example.com",
+        "profile_id" => 9
+    ],
+    [
+        "text" => "Consiglio vivamente questo dottore, mi ha aiutato molto",
+        "date" => "2023-04-25 11:45:00",
+        "name" => "Luigi",
+        "surname" => "Gallo",
+        "email" => "luigi.gallo@example.com",
+        "profile_id" => 42
+    ], 
+[
+        "text" => "Ottima esperienza, il dottore è molto competente",
+        "date" => "2023-01-15 09:30:00",
+        "name" => "Mario",
+        "surname" => "Rossi",
+        "email" => "mario.rossi@example.com",
+        "profile_id" => 9
+    ],
+    [
+        "text" => "Mi sono trovato benissimo, lo consiglio a tutti",
+        "date" => "2023-02-02 14:45:00",
+        "name" => "Laura",
+        "surname" => "Bianchi",
+        "email" => "laura.bianchi@example.com",
+        "profile_id" => 42
+    ],
+    [
+        "text" => "Il dottore è stato molto gentile e professionale",
+        "date" => "2023-02-18 11:15:00",
+        "name" => "Giovanni",
+        "surname" => "Verdi",
+        "email" => "giovanni.verdi@example.com",
+        "profile_id" => 43
+    ],
+    [
+        "text" => "Mi sono sentito ascoltato e ho ricevuto un'ottima cura",
+        "date" => "2023-03-07 16:20:00",
+        "name" => "Paola",
+        "surname" => "Rizzo",
+        "email" => "paola.rizzo@example.com",
+        "profile_id" => 44
+    ],
+    [
+        "text" => "Il dottore è davvero bravo, ho risolto il mio problema",
+        "date" => "2023-03-22 10:00:00",
+        "name" => "Marco",
+        "surname" => "Esposito",
+        "email" => "marco.esposito@example.com",
+        "profile_id" => 45
+    ],
+    [
+        "text" => "Mi sono sentita accolta e in buone mani",
+        "date" => "2023-04-10 15:30:00",
+        "name" => "Sara",
+        "surname" => "Conti",
+        "email" => "sara.conti@example.com",
+        "profile_id" => 9
+    ],
+    [
+        "text" => "Consiglio vivamente questo dottore, mi ha aiutato molto",
+        "date" => "2023-04-25 11:45:00",
+        "name" => "Luigi",
+        "surname" => "Gallo",
+        "email" => "luigi.gallo@example.com",
+        "profile_id" => 42
+    ],
+    [
+        "text" => "Ho avuto una grande esperienza con questo medico",
+        "date" => "2023-05-12 14:00:00",
+        "name" => "Simona",
+        "surname" => "Mancini",
+        "email" => "luigi.gallo@example.com",
+        "profile_id" => 42
+    ]
+];
